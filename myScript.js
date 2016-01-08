b0164cd84b967c8e1fc2db76034ce0c6b19f3666
Ulises E. Cervantes Torres <ulises.cerv@hotmail.com>
2016-01-08 13:40:26 -0800

    its working
    Its work after 4 seconds

---
+++ myScript.js
@@ -0,0 +1,10 @@
+var container=document.getElementById('container');
+setTimeout(function(){
+	//add class
+	container.classList.add("cerrar");
+//9 segundos
+},4000)
+
+window.addEventListener('load',function(){
+	console.log("cargado...")
+})
